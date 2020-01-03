76fae7970e0f4b4643aef3a3fb3922b5d243b1df
Stephen High <shigh@ucdavis.edu>
2020-01-02 21:21:46 -0800

    add argument options class

---
+++ bulk-attachment-delete/Options.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace bulk_attachment_delete
+{
+    public class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Path to CSV input file containing attachment file names.")]
+        public string FilePath { get; set; }
+
+        [Option('h', "header", Default = true, HelpText = "CSV input file has a header row.")]
+        public bool HasHeaderRecord { get; set; }
+
+        [Option(Default = false, HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+    }
+}
