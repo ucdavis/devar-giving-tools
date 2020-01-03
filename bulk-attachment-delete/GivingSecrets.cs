8694ff25d078be3d646171fbbfa6400b3200fb94
Stephen High <shigh@ucdavis.edu>
2020-01-02 21:21:08 -0800

    add giving user secrets

---
+++ bulk-attachment-delete/GivingSecrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bulk_attachment_delete
+{
+    public class GivingSecrets
+    {
+        public string ApiKey { get; set; }
+
+        public string ApiUrl { get; set; }
+    }
+}
